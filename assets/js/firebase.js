12f502e8317a44ab89668341f816249946fcc493
sherinkr <sherinkrply@gmail.com>
2021-08-10 09:34:25 +0530

    Firebase DB Connnected

---
+++ assets/js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyASTX-lBKvBDSAHyT8nAslYeO7ssMucZj8",
+    authDomain: "r-k-products.firebaseapp.com",
+    projectId: "r-k-products",
+    storageBucket: "r-k-products.appspot.com",
+    messagingSenderId: "119522870792",
+    appId: "1:119522870792:web:6faf71eb38332f9963f3bd",
+    measurementId: "G-SZE2QLZTJZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
